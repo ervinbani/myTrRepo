4400b76fa2cdb21bd4199c79a27d9c97f96792b8
ervinbani <bani.ervin@gmail.com>
2018-10-22 15:22:35 +0200

    Added new component

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 $(document).ready(function(){
 
 $("#idbtn").click(function(){
-  var c=20;
-  for(var x=17;x<32;x++){
+  var c=1000;
+  for(var x=0;x<32;x++){
     $("#idData").append("<div class=border >" + x + "</div>");
 
     $("#idCapitale").append("<div class=border>" + parseFloat(c).toFixed(2) + "</div>");
 
     //c=parseFloat(c + c*0.1).toFixed(2);;
-    c=c + c*0.1
+    c=c + c*0.03;
 
   }
 
