a0b8489eda9f2b99bb96f09caef4609aa3bfda02
ervinbani <bani.ervin@gmail.com>
2018-10-25 17:46:00 +0200

    style optimization

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 
 $(document).ready(function(){
   
-$("#idbtn").click(function(){
-  $("#idData").html("");
-  $("#idCapitale").html("");
-  
-  var c=parseInt($("#idCapIniziale").val());
-  if(c<0||c==0){
-      alert("Valore non valido, inserisci un numero positivo");
+    $("#idbtn").click(function(){
+        $("#idData").html("");
+        $("#idCapitale").html("");
+        
+        var c=parseInt($("#idCapIniziale").val());
+        if(c<0||c==0){
+            alert("Valore non valido, inserisci un numero positivo");
 
-  }
-  else{
-    for(var x=0;x<32;x++){
-      $("#idData").append("<div class=border >" + x + "</div>");
+        }
+        else{
+            for(var x=0;x<32;x++){
+                $("#idData").append("<div class=border >" + x + "</div>");
+                $("#idCapitale").append("<div class=border>" + parseFloat(c).toFixed(2) + "</div>");
 
-        //c=parseFloat(c + c*0.1).toFixed(2);;
-      
-      $("#idCapitale").append("<div class=border>" + parseFloat(c).toFixed(2) + "</div>");
+                c=c + c*0.03;
 
-      c=c + c*0.03;
+          }
+        }
+        
+        $("#idCapIniziale").val("");
 
-    }
-  }
-  
-  $("#idCapIniziale").val("");
-
-
-});
-
+    });
 
 });
