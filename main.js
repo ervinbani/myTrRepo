29dda70018875a0adb1ae7356b8dd92198fc8e72
ervinbani <bani.ervin@gmail.com>
2018-10-25 17:26:22 +0200

    added new function

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 
 $(document).ready(function(){
-
+  
 $("#idbtn").click(function(){
-  var c=1000;
+  $("#idData").html("");
+  $("#idCapitale").html("");
+  
+  var c=parseInt($("#idCapIniziale").val());
   for(var x=0;x<32;x++){
     $("#idData").append("<div class=border >" + x + "</div>");
 
+      //c=parseFloat(c + c*0.1).toFixed(2);;
+    
     $("#idCapitale").append("<div class=border>" + parseFloat(c).toFixed(2) + "</div>");
 
-    //c=parseFloat(c + c*0.1).toFixed(2);;
     c=c + c*0.03;
 
   }
+  
+  $("#idCapIniziale").val("");
 
 
 });
-
-
-
-
-
 
 
 });
