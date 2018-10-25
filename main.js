a6837dc17ba9734537779d036391b2bfc9f94ed3
ervinbani <bani.ervin@gmail.com>
2018-10-25 17:42:17 +0200

    added new for contition

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ $("#idbtn").click(function(){
   $("#idCapitale").html("");
   
   var c=parseInt($("#idCapIniziale").val());
-  for(var x=0;x<32;x++){
-    $("#idData").append("<div class=border >" + x + "</div>");
+  if(c<0||c==0){
+      alert("Valore non valido, inserisci un numero positivo");
 
-      //c=parseFloat(c + c*0.1).toFixed(2);;
-    
-    $("#idCapitale").append("<div class=border>" + parseFloat(c).toFixed(2) + "</div>");
+  }
+  else{
+    for(var x=0;x<32;x++){
+      $("#idData").append("<div class=border >" + x + "</div>");
 
-    c=c + c*0.03;
+        //c=parseFloat(c + c*0.1).toFixed(2);;
+      
+      $("#idCapitale").append("<div class=border>" + parseFloat(c).toFixed(2) + "</div>");
 
+      c=c + c*0.03;
+
+    }
   }
   
   $("#idCapIniziale").val("");
